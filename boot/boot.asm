d91c881bd3af0fdf512d58d7df8773e7fae6dbfc
wys <wangyisong1996@126.com>
2020-03-09 14:50:31 +0800

    setup 64-bit gdt

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -12,9 +13,10 @@ start:
     call set_up_page_tables
     call enable_paging
 
-    ; print `OK` to screen
-    mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+    ; load the 64-bit GDT
+    lgdt [gdt64.pointer]
+
+    jmp gdt64.code:long_mode_start
 
 
 ; Prints `ERR: ` and the given error code to screen and hangs.
@@ -141,6 +143,16 @@ enable_paging:
     mov cr0, eax
 
     ret
+
+
+section .rodata
+gdt64:
+    dq 0 ; zero entry
+.code: equ $ - gdt64
+    dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+    dw $ - gdt64 - 1
+    dq gdt64
 
 
 section .bss
